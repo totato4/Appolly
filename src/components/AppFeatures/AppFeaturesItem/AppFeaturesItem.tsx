a25bca30dbc78ad87625f315bc228d2a14af4a1e
totato4 <v4.97@list.ru>
2023-07-18 09:22:14 +0300

    features page refactoring

--- src/components/AppFeatures/AppFeaturesItem/AppFeaturesItem.tsx
+++ src/components/AppFeatures/AppFeaturesItem/AppFeaturesItem.tsx
@@ -1,22 +1,22 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import style from "./AppFeaturesItem.module.scss";
 
 const AppFeaturesItem = ({
   title,
   text,
   svg,
+  children,
 }: {
   title: string;
   text: string;
-  svg: string;
+  svg?: string;
+  children?: ReactNode;
 }) => {
   return (
     // @ts-ignore
-    <div>
-      <div>
-        <img className={style.svg} src={`${svg}`} alt="img" />
-      </div>
-      <h5>{title}</h5>
+    <div className={style.wrapper}>
+      <div className={style.svg}>{children && children}</div>
+      <h5>{title.toUpperCase()}</h5>
       <span>{text}</span>
     </div>
   );
